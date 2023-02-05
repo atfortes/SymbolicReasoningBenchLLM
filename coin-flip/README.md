0d4c8b3611ca9b9c49037c73a583397f5f9e809b
atfortes <atfortes@protonmail.com>
2023-02-05 12:13:03 +0000

    Add Coin Flip

---
+++ coin-flip/README.md
@@ -0,0 +1,23 @@
+# Coin Flip
+
+Dataset generation code was adapted from: 
+```bibtex
+@article{Kojima2022LargeLM,
+  title={Large Language Models are Zero-Shot Reasoners},
+  author={Takeshi Kojima and Shixiang Shane Gu and Machel Reid and Yutaka Matsuo and Yusuke Iwasawa},
+  journal={ArXiv},
+  year={2022},
+  volume={abs/2205.11916}
+}
+```
+
+Dataset reference:
+```bibtex
+@article{Wei2022ChainOT,
+  title={Chain of Thought Prompting Elicits Reasoning in Large Language Models},
+  author={Jason Wei and Xuezhi Wang and Dale Schuurmans and Maarten Bosma and Ed Huai-hsin Chi and Quoc Le and Denny Zhou},
+  journal={ArXiv},
+  year={2022},
+  volume={abs/2201.11903}
+}
+```
---
+++ coin-flip/coin_flip.py
@@ -0,0 +1,47 @@
+from names_dataset import NameDataset
+from collections import OrderedDict
+import random
+import argparse
+import json
+import os
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate Coin Flip Dataset")
+    parser.add_argument("--random_seed", type=int, default=0)
+    parser.add_argument("--dataset_size", type=int, default=10)
+    parser.add_argument("--names_in_sample", type=int, default=4)
+    parser.add_argument("--data_dir", type=str, default=".")
+    parser.add_argument("--file_name", type=str, default="coin_flip")
+    args = parser.parse_args()
+
+    nd = NameDataset()
+    name_list = nd.get_top_names(n=int(args.dataset_size * args.names_in_sample / 2),
+                                 country_alpha2='US')
+    name_list = name_list['US']['M'] + name_list['US']['F']
+    random.Random(args.random_seed).shuffle(name_list)
+
+    samples = []
+    for i in range(args.dataset_size):
+        q = "A coin is heads up. "
+        heads_up = True
+        for j in range(args.names_in_sample):
+            whether_flips = bool(random.getrandbits(1))
+            heads_up = not heads_up if whether_flips else heads_up
+            k = i*args.names_in_sample + j
+            text = "flips" if whether_flips == 1 else "does not flip"
+            q += f"{name_list[k]} {text} the coin. "
+        q += "Is the coin still heads up?"
+        a = 'yes' if heads_up else 'no'
+
+        dic = OrderedDict()
+        dic["question"] = q
+        dic["answer"] = a
+        samples.append(dic)
+
+    with open(os.path.join(args.data_dir, args.file_name) + '.json', 'w') as f:
+        json.dump(samples, f, indent=4)
+
+
+if __name__ == "__main__":
+    main()
